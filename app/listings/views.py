b288edcd6d1f51cdd204d04b5242c4b2ea7cd82a
odowin <owendias@hotmail.fr>
2025-12-30 21:52:24 +0100

    Add about and listings templates; refactor views to render HTML

--- app/listings/views.py
+++ app/listings/views.py
@@ -19,19 +19,26 @@ def hello(request):
     bands = Band.objects.all()
     return render(request, 'listings/hello.html', {'bands': bands})
 
+# def about(request):
+#     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
 def about(request):
-    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+    return render(request, 'listings/about.html')
+
+# def listings(request):
+#     all_listings = Listing.objects.all()  # récupère tous les objets Listing
+#     listings_html = "<ul>"
+#     for listing in all_listings:
+#         listings_html += f"<li>{listing.title}</li>"  # crée un <li> pour chaque title
+#     listings_html += "</ul>"
+
+#     return HttpResponse(f"""
+#         <h1>Listings</h1>
+#         <p>Voici toutes les annonces :</p>
+#         {listings_html}
+#     """) # cette vue utilise un anti-pattern
+    # cf: https://www.formation-django.fr/framework-django/vues-templates/
 
 def listings(request):
     all_listings = Listing.objects.all()  # récupère tous les objets Listing
-    listings_html = "<ul>"
-    for listing in all_listings:
-        listings_html += f"<li>{listing.title}</li>"  # crée un <li> pour chaque title
-    listings_html += "</ul>"
-
-    return HttpResponse(f"""
-        <h1>Listings</h1>
-        <p>Voici toutes les annonces :</p>
-        {listings_html}
-    """) # cette vue utilise un anti-pattern
-    # cf: https://www.formation-django.fr/framework-django/vues-templates/
+    return render(request, 'listings/listings.html', {'listings': all_listings})
