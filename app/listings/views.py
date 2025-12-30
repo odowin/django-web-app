02c42f5a5cf4cc2d8384ed773d5e88a756fd8374
odowin <owendias@hotmail.fr>
2025-12-30 02:51:21 +0100

    Refactor hello view to use template rendering; add hello.html for displaying band names

--- app/listings/views.py
+++ app/listings/views.py
@@ -1,18 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from listings.models import Band, Listing
+from django.shortcuts import render
+
+# def hello(request):
+#     bands = Band.objects.all()
+#     return HttpResponse(f"""
+#         <h1>Hello Django !</h1>
+#         <p>Mes groupes préférés sont :<p>
+#         <ul>
+#             <li>{bands[0].name}</li>
+#             <li>{bands[1].name}</li>
+#             <li>{bands[2].name}</li>
+#         </ul>
+# """) # cette vue utilise un anti-pattern
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Mes groupes préférés sont :<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-""") # cette vue utilise un anti-pattern
+    return render(request, 'listings/hello.html', {'bands': bands})
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
