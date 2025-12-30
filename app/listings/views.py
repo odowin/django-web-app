3498164ee3bedd61ed2d7eaba6dc4ab51abca3a6
odowin <owendias@hotmail.fr>
2025-12-30 02:33:24 +0100

    Enhance views to display band names and listings in HTML format

--- app/listings/views.py
+++ app/listings/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+def listings(request):
+    return HttpResponse('<h1>Listings</h1> <p>Nous afficherons éventuellement une liste des annonces pour les articles !</p>')
