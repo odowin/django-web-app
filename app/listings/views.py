7645a195d00ee30a65df401bb584f21505930d33
odowin <owendias@hotmail.fr>
2025-12-30 02:33:40 +0100

    Add Band and Listing models with initial migrations; update views to display band names and listings

--- app/listings/views.py
+++ app/listings/views.py
@@ -1,11 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band, Listing
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes groupes préférés sont :<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+""") # cette vue utilise un anti-pattern
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
 
 def listings(request):
-    return HttpResponse('<h1>Listings</h1> <p>Nous afficherons éventuellement une liste des annonces pour les articles !</p>')
+    all_listings = Listing.objects.all()  # récupère tous les objets Listing
+    listings_html = "<ul>"
+    for listing in all_listings:
+        listings_html += f"<li>{listing.title}</li>"  # crée un <li> pour chaque title
+    listings_html += "</ul>"
+
+    return HttpResponse(f"""
+        <h1>Listings</h1>
+        <p>Voici toutes les annonces :</p>
+        {listings_html}
+    """) # cette vue utilise un anti-pattern
+    # cf: https://www.formation-django.fr/framework-django/vues-templates/
